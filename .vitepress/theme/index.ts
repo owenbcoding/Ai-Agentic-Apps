629749ed6501cd3fd541b1ed25a6bb1a06c0d892
owenbcoding <eoghankb@gmail.com>
2026-08-02 02:10:42 +0100

    Integrate HeroVideo component into theme layout for enhanced home hero display

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,4 +1,12 @@
 import DefaultTheme from 'vitepress/theme'
+import { h } from 'vue'
+import HeroVideo from './HeroVideo.vue'
 import './custom.css'
 
-export default DefaultTheme
+export default {
+  extends: DefaultTheme,
+  Layout: () =>
+    h(DefaultTheme.Layout, null, {
+      'home-hero-image': () => h(HeroVideo),
+    }),
+}

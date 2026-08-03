f8838896383d026c2b04b301dede5178c951251f
owenbcoding <eoghankb@gmail.com>
2026-08-03 03:24:59 +0100

    Add "Star on GitHub" component to navigation bar

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,6 +1,7 @@
 import DefaultTheme from 'vitepress/theme'
 import { h } from 'vue'
 import HeroVideo from './HeroVideo.vue'
+import StarOnGitHub from './StarOnGitHub.vue'
 import './custom.css'
 
 export default {
@@ -8,5 +9,9 @@ export default {
   Layout: () =>
     h(DefaultTheme.Layout, null, {
       'home-hero-image': () => h(HeroVideo),
+      // Rendered after the appearance toggle + social links in the DOM;
+      // custom.css reorders it (via flexbox `order`) to sit between them.
+      'nav-bar-content-after': () => h(StarOnGitHub),
+      'nav-screen-content-after': () => h(StarOnGitHub),
     }),
 }
